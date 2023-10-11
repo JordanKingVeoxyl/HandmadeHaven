4067b44da4d8bf5c29b59b12599544a0d2a4532c
JordanKingVeoxyl <veoxyl@gmail.com>
2023-10-11 18:48:04 +0000

    configured testing for django

--- todo/test_views.py
+++ todo/test_views.py
@@ -44,3 +44,4 @@ class TestDjango(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertEqual(updated_item.name, 'Updated Name')
+        
